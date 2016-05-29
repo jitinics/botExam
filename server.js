f846c5d58c2ff2ef1896464cf99d8e658d38d5c7
Jittaranan Tipboonde <jitinics@NICs-Jittaranans-MacBook-Air.local>
2016-05-29 09:39:43 +0700

    +

--- server.js
+++ server.js
@@ -18,7 +18,18 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-  console.log(req.body)
+  var messaging_events = req.body.entry[0].messaging
+  var event
+  var sender
+  var text
+  for (var i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.id
+    if (event.message && event.message.text) {
+      text = event.message.text
+      console.log(text)
+    }
+  }
   res.sendStatus(200)
 })
 
